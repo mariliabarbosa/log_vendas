859fc37cc4596f7fba44a544fa6f384966484fc9
Rafaela Barros <82919168+RafaelaBarros@users.noreply.github.com>
2021-12-02 19:58:03 -0300

    Adicionada lógica de cálculo
    Adicionado o pacote para lógica de cálculo com Python e Sympy

---
+++ piCalcs/diferencial.py
@@ -0,0 +1,43 @@
+from sympy import *
+
+q = symbols('q')
+
+def funcaoCusto(q):
+    return 0.03*q**3 - 1.8*q**2 + 39*q
+
+
+class Financeiro:
+    def __init__(self, pVenda):
+        self.custos = funcaoCusto(q)
+        self.vendas = 0
+        self.precoVenda = pVenda
+        self.lucro = self.precoVenda*q - self.custos
+
+    def raizes(self):
+        diferencial1 = self.lucro.diff(q)
+        a = solve(diferencial1, q)
+
+        return a
+
+    def calcMax(self):
+        diferencial2 = self.lucro.diff(q).diff(q)
+        raizesQ = self.raizes()
+
+        listaValRaiz = {}
+
+        valMin = []
+
+        valMax = []
+
+        for raiz in raizesQ:
+            valor = diferencial2.subs(q,raiz)
+            
+            listaValRaiz[valor] = raiz
+        
+        for valor in listaValRaiz.keys():
+            if valor > 0:
+                valMin.append(listaValRaiz[valor])
+            elif valor < 0:
+                valMax.append(listaValRaiz[valor])
+
+        return valMax
